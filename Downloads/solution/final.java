e92f3caade70cad137c1df10ab81dc0baaa907ad
Akshat Mishra <akshatmishra@Akshats-MacBook-Pro.local>
2015-10-31 15:49:32 -0700

    pushing compilation error in final java

---
+++ Downloads/solution/final.java
@@ -0,0 +1,15 @@
+package mypackage;
+
+public class final {
+	
+	public static void main(String args[]) {
+		
+		final String x="firstname";
+		System.out.println(x);
+		x="lastname";
+		
+	}
+	
+
+}
+//compilation fails in this case
